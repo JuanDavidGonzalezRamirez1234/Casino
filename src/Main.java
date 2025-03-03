3eec35e3f417ea3bbf07d54204a9e68833a8972b
Juan Gonzalez <juan.gonzalez-ram@uniminuto.edu.co>
2025-03-03 08:55:45 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+        import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner Teclado =  new Scanner(System.in);
+        int dinero;
+
+        System.out.println("ingrese su dinero");
+        dinero = Teclado.nextInt();
+
+        
+
+
+
+
+
+
+
+
+
+
+    }
+}
